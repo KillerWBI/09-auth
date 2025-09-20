52004fd1c5506af8a0f234cbf36c3251e1ca07cd
GetUp <108660418+KillerWBI@users.noreply.github.com>
2025-09-20 14:49:07 +0200

    цв

--- app/notes/[id]/error.tsx
+++ app/notes/[id]/error.tsx
@@ -1,8 +1,7 @@
-
+"use client";
 
 function Error({ error }: { error: Error }) {
-  return <p>Could not fetch note details. {error.message}</p>
-;
+  return <p>Could not fetch note details. {error.message}</p>;
 }
 
 export default Error;
