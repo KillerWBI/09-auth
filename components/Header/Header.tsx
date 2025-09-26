33e6c04b2588dfd6264da9a04fcc8f2f6f1375e9
GetUp <108660418+KillerWBI@users.noreply.github.com>
2025-09-26 15:16:53 +0200

    we

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
+import TagsMenu from '../TagsMenu/TagsMenu';
 import css from './Header.module.css';
-import TagsMenu from './TagsMenu';
 
 const Header = () => {
   return (
